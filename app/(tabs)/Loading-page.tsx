49879176034c70acaaa70142ccea28b45108c0fc
ArjunSNair00 <snairarjun43@gmail.com>
2026-01-13 23:32:54 +0530

    tabss working

---
+++ app/(tabs)/Loading-page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const LoadingPage = () => {
+  return (
+    <View>
+      <Text>Loading-page</Text>
+    </View>
+  );
+};
+
+export default LoadingPage;
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,44 @@
 import { Tabs } from "expo-router";
 
-export default function Tabslayout() {
-  //consistent components
+export default function TabsLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ headerShown: false }} />
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "#007AFF",
+        tabBarInactiveTintColor: "#999",
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: "600",
+        },
+        tabBarStyle: {
+          paddingBottom: 5,
+          paddingTop: 5,
+          height: 60,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="Login"
+        options={{
+          tabBarLabel: "Login",
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="Loading-page"
+        options={{
+          tabBarLabel: "Loading",
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="Home"
+        options={{
+          tabBarLabel: "Home",
+          headerShown: false,
+        }}
+      />
     </Tabs>
   );
 }
