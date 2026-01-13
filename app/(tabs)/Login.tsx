a26963b1613d8556b4942fd0267f17cb9569cc5b
ArjunSNair00 <snairarjun43@gmail.com>
2026-01-14 00:32:01 +0530

    welcome

--- app/(tabs)/Login.tsx
+++ app/(tabs)/Login.tsx
@@ -1,10 +1,19 @@
+import { useFonts } from "expo-font";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 const Login = () => {
+  const [fontsLoaded] = useFonts({
+    BungeeInline: require("../../assets/Fonts/BungeeInline-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Email: </Text>
+      <Text style={styles.Welcome}>Welcome</Text>
     </View>
   );
 };
@@ -16,5 +25,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    borderWidth: 5,
+    borderColor: "#b41a1a",
+  },
+  Welcome: {
+    color: "#FF00BB",
+    fontFamily: "BungeeInline",
+    fontSize: 48,
   },
 });
