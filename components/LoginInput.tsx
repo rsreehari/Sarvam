a0176262a3275e57242e0de081d63d2de829122b
ArjunSNair00 <snairarjun43@gmail.com>
2026-01-14 00:35:02 +0530

    login input

---
+++ components/LoginInput.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const LoginInput = () => {
+  return (
+    <View>
+      <Text>LoginInput</Text>
+    </View>
+  );
+};
+
+export default LoginInput;
